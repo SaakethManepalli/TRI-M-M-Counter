5614ea59e85681ed6203d5613ad2d9aceabd154a
Saaketh M <saaketh.manepalli@gmail.com>
2023-05-17 19:04:10 -0400

    god.py is second third attempt at making this work, reworking with ONLY my code

--- god.py
+++ god.py
@@ -1,23 +1,38 @@
-from logging import exception
+import traceback
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
+from oauth2client import client, file, tools
+from googleapiclient.errors import HttpError
+import httplib2
+import pandas
+import json
 
-# import json
-
-flow = InstalledAppFlow.from_client_secrets_file('/Users/saakethmanepalli/Desktop/Intro Comp Sci/cred/credentials.json',
-                                                 ['https://www.googleapis.com/auth/forms',
-                                                  'https://www.googleapis.com/auth/spreadsheets'])
-# don't know how this works
-creds = flow.run_local_server(port=0)
+httplib2.debuglevel = 4
+scopes = ['https://www.googleapis.com/auth/forms', 'https://www.googleapis.com/auth/spreadsheets',
+          'https://www.googleapis.com/auth/drive']
+store = file.Storage('token.json')
+creds = None
+if not creds:
+    flow = client.flow_from_clientsecrets('client_secrets.json', scopes)
+    creds = tools.run_flow(flow, store)
 
 # build forms instance
 forms = build('forms', 'v1', credentials=creds)
+urmom = build('sheets', 'v4', credentials=creds)
 formID = input("Enter Form ID")
-
-
 
 try:
     responses = forms.forms().responses().list(formId=formID).execute()
-except exception as e:
+    with open('form_responses.json', 'w') as f:
+        json.dump(responses, f)
+except HttpError:  # Cannot try/except non BaseException
     print(f"Error retrieving {formID} Data")
+    print(traceback)
+
+'''
+begin the json-ing to sheet-ing
+'''
+with open('form_responses.json', 'r') as f:
+    helpme = json.load(f)
+    df = pandas.DataFrame.from_dict(helpme)
+    print(df)
 
